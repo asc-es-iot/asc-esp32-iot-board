7973515c1238c201c63636295fd5fbc78e68c83a
Ananthakrishnan Balajee <86157960+ananthakrishna7@users.noreply.github.com>
2023-08-25 16:14:43 +0530

    Update ldr.ino
    Add Comments

--- basics/ldr/ldr.ino
+++ basics/ldr/ldr.ino
@@ -1,11 +1,11 @@
 void setup()
 {
-  pinMode (39, INPUT);
-  Serial.begin(115200);
+  pinMode (39, INPUT); // LDR is on Pin 39
+  Serial.begin(115200); // init Serial at 115200 baud
 }
 void loop()
 {
-  int ldrReading = analogRead(39);
-  Serial.println(ldrReading);
-  delay(500);
-}
+  int ldrReading = analogRead(39); // read from the LDR on pin 39 
+  Serial.println(ldrReading); // send reading over serial
+  delay(500); // wait 500ms
+} // repeat
